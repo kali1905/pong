be1fbd879ce18f1352a0a7b1aa87ee83279ecb7e
kali1905 <sinaahmet@gmail.com>
2025-06-28 18:30:57 +0300

    paddles updated

--- main.lua
+++ main.lua
@@ -6,9 +6,12 @@ WINDOW_HEIGHT = 1080
 VIRTUAL_WIDTH = 648
 VIRTUAL_HEIGHT = 364
 
+PADDLE_SPEED = 200
+
 function love.load()
     love.graphics.setDefaultFilter('nearest', 'nearest')
     smallFont = love.graphics.newFont('font.ttf', 8)
+    scoreFont = love.graphics.newFont('font.ttf', 32)
     love.graphics.setFont(smallFont)
 
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
@@ -16,6 +19,26 @@ function love.load()
         resizable = false,
         vsync = true
     })
+    
+    player1Score = 0
+    player2Score = 0
+
+    player1Y = 30
+    player2Y = VIRTUAL_HEIGHT - 100
+end
+
+function love.update(dt)
+    if love.keyboard.isDown('w') then
+        player1Y = player1Y + -PADDLE_SPEED * dt
+    elseif love.keyboard.isDown('s') then
+        player1Y = player1Y + PADDLE_SPEED * dt
+    end
+
+    if love.keyboard.isDown('up') then
+        player2Y = player2Y + -PADDLE_SPEED * dt
+    elseif love.keyboard.isDown('down') then
+        player2Y = player2Y + PADDLE_SPEED * dt
+    end
 end
 
 function love.keypressed(key)
@@ -27,11 +50,19 @@ end
 function love.draw()
     push:apply('start')
     love.graphics.clear(40/255, 45/255, 52/255, 255/255)
-        
-    love.graphics.rectangle('fill', 10, 30, 5, 50)
-    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 100, 5, 50)    
-    love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - 5, VIRTUAL_HEIGHT / 2 - 25, 5, 5)
+    love.graphics.setFont(smallFont)
     
+    love.graphics.printf('Aşkımla Pong Oynarız!', 0, 20, VIRTUAL_WIDTH, 'center')
+    
+    love.graphics.setFont(scoreFont)
+    love.graphics.print(tostring(player1Score), VIRTUAL_WIDTH / 2 - 50, VIRTUAL_HEIGHT /3)
+    love.graphics.print(tostring(player2Score), VIRTUAL_WIDTH / 2 + 30, VIRTUAL_HEIGHT /3)
+    
+    
+    
+    love.graphics.rectangle('fill', 10, player1Y, 5, 50)
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, player2Y, 5, 50)    
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - 5, VIRTUAL_HEIGHT / 2 - 25, 5, 5)
     
     push:apply('end')
 end
