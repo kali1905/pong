8ea37dfb043d1f510e5d14a1dd2fc15f4ffa7e14
kali1905 <sinaahmet@gmail.com>
2025-06-28 16:22:46 +0300

    Second

--- main.lua
+++ main.lua
@@ -1,6 +1,30 @@
+push = require 'push'
+
 WINDOW_WIDTH = 1920
 WINDOW_HEIGHT = 1080
 
-function love.window.setMode(1920 ,1080p , FullScreen)
+VIRTUAL_WIDTH = 648
+VIRTUAL_HEIGHT = 364
 
+function love.load()
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+        fullscreen = false,
+        resizable = false,
+        vsync = true
+    })
+end
+
+function love.draw()
+    
+    
+end
+
+function love.draw()
+    love.graphics.printf(
+          'Hello Pong!',
+          0,
+          WINDOW_HEIGHT / 2 - 6,
+          WINDOW_WIDTH,
+          'center')    
 end
