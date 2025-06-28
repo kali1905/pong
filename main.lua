a9e9b2026d41b402e063d957b325cedaf5261335
kali1905 <sinaahmet@gmail.com>
2025-06-28 18:16:31 +0300

    Rectangle update

--- main.lua
+++ main.lua
@@ -8,6 +8,9 @@ VIRTUAL_HEIGHT = 364
 
 function love.load()
     love.graphics.setDefaultFilter('nearest', 'nearest')
+    smallFont = love.graphics.newFont('font.ttf', 8)
+    love.graphics.setFont(smallFont)
+
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
         resizable = false,
@@ -23,6 +26,13 @@ end
 
 function love.draw()
     push:apply('start')
-    love.graphics.printf('Hello Ghardsh?', 0, VIRTUAL_HEIGHT / 2 - 6, VIRTUAL_WIDTH, 'center')
+    love.graphics.clear(40/255, 45/255, 52/255, 255/255)
+        
+    love.graphics.rectangle('fill', 10, 30, 5, 50)
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 100, 5, 50)    
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - 5, VIRTUAL_HEIGHT / 2 - 25, 5, 5)
+    
+    
     push:apply('end')
 end
+
